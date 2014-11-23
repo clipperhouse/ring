1f2917320b6b0e52af73a93633a1e427d9dd9e73
Matt Sherman <mwsherman@gmail.com>
2014-11-22 19:29:51 -0500

    initial commit

---
+++ ringwriter.go
@@ -0,0 +1,60 @@
+package ringwriter
+
+import (
+	"io"
+
+	"github.com/clipperhouse/typewriter"
+)
+
+func init() {
+	err := typewriter.Register(NewRingWriter())
+	if err != nil {
+		panic(err)
+	}
+}
+
+type RingWriter struct{}
+
+func NewRingWriter() *RingWriter {
+	return &RingWriter{}
+}
+
+func (sw *RingWriter) Name() string {
+	return "ring"
+}
+
+func (sw *RingWriter) Imports(t typewriter.Type) (result []typewriter.ImportSpec) {
+	// none
+	return result
+}
+
+func (sw *RingWriter) Write(w io.Writer, t typewriter.Type) error {
+	tag, found := t.FindTag(sw)
+
+	if !found {
+		// nothing to be done
+		return nil
+	}
+
+	license := `
+// Ring is a modification of http://golang.org/pkg/container/ring/
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+`
+	if _, err := w.Write([]byte(license)); err != nil {
+		return err
+	}
+
+	tmpl, err := templates.ByTag(t, tag)
+
+	if err != nil {
+		return err
+	}
+
+	if err := tmpl.Execute(w, t); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ test/thing.go
@@ -0,0 +1,4 @@
+package main
+
+// +test ring
+type Thing int
